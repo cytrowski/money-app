0fa54f41be6bed958a097b98239eb2f8337dc482
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-02-15 17:43:54 +0100

    Create grid template, need some configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './money-app/styles/main.css';
 import './money-app/styles/navbar/navbar.css';
+import './money-app/styles/budget/budget.css';
+import './money-app/styles/expense_form/expense.css';
+import './money-app/styles/table/table.css';
+import './money-app/styles/stats/stats.css';
 import Navbar from './money-app/navbar';
 
 // animate login cart
--- src/money-app/navbar.js
+++ src/money-app/navbar.js
@@ -5,17 +5,15 @@ class Navbar {
         this.login = container.querySelectorAll('.slide_menu');
     }
     init(){
-
         this.nav.forEach(nav => {
             nav.addEventListener('click', e => {
             if(e.target.tagName === 'LI'){
-                // this.login.classList.toggle('active');
                 this.slideMenu(e);
             };
         });
     });
     }
-    slideMenu(e){
+    slideMenu(e){ //add class active
          const selector = e.target.getAttribute('data-target');
          const content = this.container.querySelector(selector);
          content.classList.toggle('active');
