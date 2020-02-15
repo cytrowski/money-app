cdbb94c79623e8cd72166b0c06e70f42a982db74
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-02-15 19:55:02 +0100

    make firebase connection

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import './money-app/styles/expense_form/expense.css';
 import './money-app/styles/table/table.css';
 import './money-app/styles/stats/stats.css';
 import Navbar from './money-app/navbar';
+import Product from './money-app/product';
 
 // animate login cart
 const showLogin = new Navbar(document.querySelector('.navbar'));
 showLogin.init();
+
+
+//testing products
+
+const products = new Product('pierogi', '22,39');
+products.getProducts((data) => {
+    console.log(data);
+});
---
+++ src/money-app/product.js
@@ -0,0 +1,32 @@
+class Product {
+    constructor() {
+        this.products = db.collection('money-app');
+    } 
+    async addProduct(name, price) { //dodaje produkt do firebase
+        const now = new Date();
+        const product = {
+            name: name,
+            price: price,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        const response = await this.products.add(product);
+        return response;
+    }
+    getProducts(callback){ //pobiera liste z firebase
+        this.products
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added'){
+                        //udpate UI
+                        callback(change.doc.data());
+                    };
+                });
+        });
+    }
+}
+
+
+
+
+
+export { Product as default };
