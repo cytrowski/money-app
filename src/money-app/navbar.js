72bd53f77b289eef1a4edb60f46417b5978706c1
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-02-15 14:46:16 +0100

    Finish navbar

--- src/money-app/navbar.js
+++ src/money-app/navbar.js
@@ -1,18 +1,26 @@
 class Navbar {
     constructor (container){
         this.container = container;
-        this.nav = container.querySelector('.nav__pick');
-        this.login = container.querySelector('.login');
+        this.nav = container.querySelectorAll('.nav__pick');
+        this.login = container.querySelectorAll('.slide_menu');
     }
     init(){
 
-        this.nav.addEventListener('click', e => {
+        this.nav.forEach(nav => {
+            nav.addEventListener('click', e => {
             if(e.target.tagName === 'LI'){
-                this.login.classList.toggle('active');
+                // this.login.classList.toggle('active');
+                this.slideMenu(e);
             };
         });
-
+    });
     }
+    slideMenu(e){
+         const selector = e.target.getAttribute('data-target');
+         const content = this.container.querySelector(selector);
+         content.classList.toggle('active');
+    }
+    
 }
 
 export { Navbar as default };
